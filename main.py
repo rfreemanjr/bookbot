fa2ff2526ae55ecd86e2aa4e23bc86ab2477a1b6
rfreemanjr <rdfreejr@gmail.com>
2024-03-18 22:18:44 -0400

    Added letter count method

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ def main():
     file = "./books/frankenstein.txt"
     book = get_book(file)
     words = get_word_count(book)
-    print(f"There are {words} words in this  document")
+    letters = get_letter_count(book)
+    print(f"There are {words} words in this  document and \n {letters} are the unique letter counts")
 
 #This method will get the book
 def get_book(book_path):
@@ -16,6 +17,18 @@ def get_book(book_path):
 #This method will return the word count associated with the book.
 def get_word_count(book):
     return len(book.split())
+
+#this method gets the letter count of each letter"
+def get_letter_count(book):
+    letter_dict = {
+        'a': 0,'b': 0,'c': 0,'d': 0,'e': 0,'f': 0,'g': 0,'h': 0,'i': 0,'j': 0,'k': 0,'l': 0,'m': 0,'n': 0,'o': 0,'p': 0,
+        'q': 0,'r': 0,'s': 0,'t': 0,'u': 0,'v': 0,'w': 0,'x': 0,'y': 0,'z': 0
+    }
+    for letter in book:
+        char = letter.lower()
+        if char in letter_dict:
+            letter_dict[char] += 1
+    return letter_dict
 
 #Executes the main method of the program
 main()
